c82240dc5ee832353108eb73e6a2aaaed93f6eb4
Matthew Styzinski <75954940+GiffenGood1@users.noreply.github.com>
2021-05-18 14:35:55 +1000

    user/update end point and request to auth0 working (#8)

--- src/routes/users-router.js
+++ src/routes/users-router.js
@@ -1,60 +1,110 @@
-const express = require('express')
-const path = require('path')
-const UsersService = require('../services/users-service')
+const { default: axios } = require("axios");
+const express = require("express");
+const path = require("path");
+const UsersService = require("../services/users-service");
+const AuthService = require("../services/auth-service");
 
-const usersRouter = express.Router()
-const jsonBodyParser = express.json()
+const usersRouter = express.Router();
+const jsonBodyParser = express.json();
+const jwtCheck = require("../middleware/oAuth");
 
 usersRouter
-//validate email before create user
-  .post('/register', jsonBodyParser, (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body
+  //validate email before create user
+  .post("/register", jsonBodyParser, (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
 
-    for (const field of ['firstName', 'lastName', 'email', 'password'])
+    for (const field of ["firstName", "lastName", "email", "password"])
       if (!req.body[field])
         return res.status(400).json({
-          error: `Missing '${field}' in request body`
-        })
+          error: `Missing '${field}' in request body`,
+        });
 
     // TODO: check user_name doesn't start with spaces
 
-    const passwordError = UsersService.validatePassword(password)
+    const passwordError = UsersService.validatePassword(password);
 
-    if (passwordError)
-      return res.status(400).json({ error: passwordError })
+    if (passwordError) return res.status(400).json({ error: passwordError });
 
-    UsersService.hasUserWithEmail(
-      req.app.get('db'),
-      email
-    )
-      .then(hasUserWithEmail => {
+    UsersService.hasUserWithEmail(req.app.get("db"), email)
+      .then((hasUserWithEmail) => {
         if (hasUserWithEmail)
-          return res.status(400).json({ error: `Email already taken` })
+          return res.status(400).json({ error: `Email already taken` });
 
-        return UsersService.hashPassword(password)
-          .then(hashedPassword => {
-            const newUser = {
-              firstName,
-              lastName,
-              email,
-              password: hashedPassword,
-              createdAt: 'now()',
+        return UsersService.hashPassword(password).then((hashedPassword) => {
+          const newUser = {
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            createdAt: "now()",
+          };
+
+          return UsersService.insertUser(req.app.get("db"), newUser).then(
+            (user) => {
+              res
+                .status(201)
+                // userId?
+                .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                .json(UsersService.serializeUser(user));
             }
-
-            return UsersService.insertUser(
-              req.app.get('db'),
-              newUser
-            )
-              .then(user => {
-                res
-                  .status(201)
-                    // userId?
-                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                  .json(UsersService.serializeUser(user))
-              })
-          })
+          );
+        });
       })
-      .catch(next)
+      .catch(next);
   })
 
-module.exports = usersRouter
+  .post("/update", jsonBodyParser, jwtCheck, (req, res, next) => {
+    const { firstName, lastName, handle, location, timezone } = req.body;
+
+    for (const field of [
+      "firstName",
+      "lastName",
+      "handle",
+      "location",
+      "timezone",
+    ])
+      if (!req.body[field])
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`,
+        });
+
+    const data = {
+      given_name: firstName,
+      family_name: lastName,
+      name: `${firstName} ${lastName}`,
+      nickname: handle,
+      user_metadata: { location: location, timezone: timezone },
+    };
+
+    // Get a token for sending requests to the Auth0 Management Api
+    AuthService.getManagementApiJwt()
+      .then((tokenRes) => {
+        const token = tokenRes.data.access_token;
+
+        axios
+          .patch(
+            "https://isgood-webapp.us.auth0.com/api/v2/users/" + req.user.sub,
+            data,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then((managementApiRes) => {
+            res.status(201).json(managementApiRes.data);
+          })
+          .catch((err) => {
+            // error handling here needs to be looked at in the future
+            console.log(err.response.data);
+            res.status(err.response.data.statusCode).json(err.response.data);
+          });
+      })
+      .catch((err) => {
+        // error handling here needs to be looked at in the future
+        console.log(err.response);
+        res.status(err.response.status).json(err.response.data);
+      });
+  });
+
+module.exports = usersRouter;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,34 +1,44 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const config = require('../config')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const axios = require("axios");
 
 const AuthService = {
   getUserWithEmail(db, email) {
-    return db('user')
-      .where({ email })
-      .first()
+    return db("user").where({ email }).first();
   },
   comparePasswords(password, hash) {
-    return bcrypt.compare(password, hash)
+    return bcrypt.compare(password, hash);
   },
   createJwt(subject, payload) {
     return jwt.sign(payload, config.JWT_SECRET, {
       subject,
       expiresIn: config.JWT_EXPIRY,
-      algorithm: 'RS256',
-    })
+      algorithm: "RS256",
+    });
   },
   verifyJwt(token) {
     return jwt.verify(token, config.JWT_SECRET, {
-      algorithms: ['RS256'],
-    })
+      algorithms: ["RS256"],
+    });
   },
   parseBasicToken(token) {
-    return Buffer
-      .from(token, 'base64')
-      .toString()
-      .split(':')
+    return Buffer.from(token, "base64").toString().split(":");
   },
-}
+  getManagementApiJwt() {
+    const clientId = process.env.CLIENT_ID_MANAGEMENT;
+    const clientSecret = process.env.CLIENT_SECRET_MANAGEMENT;
+    const domain = process.env.AUTH0_DOMAIN;
 
-module.exports = AuthService
+    const data = {
+      client_id: clientId,
+      client_secret: clientSecret,
+      audience: domain + "/api/v2/",
+      grant_type: "client_credentials",
+    };
+
+    return axios.post(domain + "/oauth/token", data);
+  },
+};
+
+module.exports = AuthService;
