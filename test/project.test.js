23bc17b162e05528ef428f0983a51515786433c7
Michael Okolo <okolomikel2009@gmail.com>
2021-06-12 09:29:20 +1000

    Get the latest changes from the Development head

--- test/project.test.js
+++ test/project.test.js
@@ -1,12 +1,13 @@
 const supertest = require('supertest');
 const { db, Auth, app } = require('./setup')
 const { cleanTable } = require('./test_helper_functions')
+const { org, } = require('./test_data')
 
-
+app.set('db', db);
+request = supertest(app);
 // Set up the database and insert some data
-beforeAll(() => {
-    app.set('db', db);
-    request = supertest(app);
+beforeAll( async () => {
+    
 });
 
 // Destroy the Database and clear all data
@@ -16,16 +17,16 @@ afterAll(() => {
 
 // Set up Each Test with somed data
 beforeEach(() => {
- 
+    
 });
 
 // Clear all the data used for each tests
 afterEach(() => {
-
 });
 
-describe('Organisations', () => {
-    it("List all", () => {
-
+describe('Projects', () => {
+    it("Lists all Projects --> GET ", async () => {
+        const response = await request.get('/api/project').set(Auth);
+        expect(response.statusCode).toBe(200)
     });
 });
