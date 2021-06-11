75d3b06ab520091a9fa95d6fa2f188401f038835
Matt Sirkis <sirkis.matt@gmail.com>
2021-06-11 17:02:34 +1200

    delete old projectList function

--- src/routes/project/projectList.js
+++ src/routes/project/projectList.js
@@ -2,69 +2,6 @@ const ProjectService = require("../../services/project-service");
 const OrgService = require("../../services/org-service");
 const AWS_S3_Service = require("../../services/aws-s3-service");
 
-function listAllProjects(req, res, next) {
-  const userId = req.user.sub;
-
-  ProjectService.getProjectIdBasedOnUser(req.app.get("db"), userId)
-    .then((projectUser) => {
-      if (!projectUser) {
-        return res.status(400).json({
-          error: { message: `No Projects` },
-        });
-      }
-      const projectIds = projectUser.map((item) => item.project_id);
-      ProjectService.getProjects(req.app.get("db"), projectIds).then(
-        (projects) => {
-          for (let i = 0; i < projects.length; i++) {
-            for (let j = 0; j < projectUser.length; j++) {
-              if (projects[i].project_id === projectUser[j].project_id) {
-                projects[i].role = projectUser[j].name;
-              }
-            }
-          }
-          AWS_S3_Service.getProjectLogos(req.app.get("db"), projectIds).then(
-            (projectLogos) => {
-              for (let i = 0; i < projects.length; i++) {
-                for (let j = 0; j < projectLogos.length; j++) {
-                  if (projects[i].project_id === projectLogos[j].project_id) {
-                    projects[i].logo = {
-                      location: projectLogos[j].location,
-                      id: projectLogos[j].id,
-                    };
-                  } else {
-                    projects[i].logo = {};
-                  }
-                }
-              }
-
-              AWS_S3_Service.getProjectBanners(
-                req.app.get("db"),
-                projectIds
-              ).then((projectBanners) => {
-                for (let i = 0; i < projects.length; i++) {
-                  for (let j = 0; j < projectBanners.length; j++) {
-                    if (
-                      projects[i].project_id === projectBanners[j].project_id
-                    ) {
-                      projects[i].banner = {
-                        location: projectBanners[j].location,
-                        id: projectBanners[j].id,
-                      };
-                    } else {
-                      projects[i].banner = {};
-                    }
-                  }
-                }
-                res.status(200).json(projects);
-              });
-            }
-          );
-        }
-      );
-    })
-    .catch(next);
-}
-
 const listProjects = async (req, res, next) => {
   const userId = req.user.sub;
 
