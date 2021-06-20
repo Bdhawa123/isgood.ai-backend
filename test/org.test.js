93a697b1898c5084851584d782a13fe434b8bb97
Michael Okolo <okolomikel2009@gmail.com>
2021-06-20 12:28:14 +1000

    removed the abstraction for supertest

--- test/org.test.js
+++ test/org.test.js
@@ -1,20 +1,21 @@
 const supertest = require('supertest');
-const { db, Auth, app } = require('./setup')
+const { db, Auth } = require('./setup')
 const { org } = require('./test_data')
+const app = require('../src/app')
+
 const { cleanTable } = require('./test_helper_functions')
 
 
 // Set up the database and insert some data
 beforeAll(() => {
     app.set('db', db);
-    request = supertest(app);
     cleanTable("org");
     cleanTable("org_user")
 });
 
 // Destroy the Database and clear all data
-afterAll(() => {
-    db.destroy()
+afterAll( async () => {
+    await db.destroy();
 });
 
 // Set up Each Test with somed data
@@ -30,24 +31,24 @@ afterEach(() => {
 describe('Organisations', () => {
     
     it('Lists all Organisations --> GET - array(json)', async () => {
-        const response = await request.get('/api/org').set(Auth);
+        const response = await supertest(app).get('/api/org').set(Auth);
         expect(response.headers["content-type"]).toContain('application/json')
         expect(response.statusCode).toBe(200);
     });
 
     it('Creates New Organisations --> POST', async () => {
-        const response = await request.post('/api/org/create').set(Auth).send(org[0]);
+        const response = await supertest(app).post('/api/org/create').set(Auth).send(org[0]);
         expect(response.statusCode).toBe(201);
         expect(response.body).toHaveProperty("org_id")
     });
 
     it("Does not grant GET access to Unauthorised user", async () => {
-        const response = await request.get("/api/org");
+        const response = await supertest(app).get("/api/org");
         expect(response.statusCode).toBe(401)
     });
 
     it("Does not grant POST access to Unauthorised user", async () => {
-        const response = await request.post('/api/org/create').send(org[0]);
+        const response = await supertest(app).post('/api/org/create').send(org[0]);
         expect(response.statusCode).toBe(401)
     });
 });
--- test/project.test.js
+++ test/project.test.js
@@ -3,11 +3,10 @@ const { db, Auth, app } = require('./setup')
 const { cleanTable } = require('./test_helper_functions')
 const { org, project, } = require('./test_data');
 
-app.set('db', db);
-request = supertest(app);
 // Set up the database and insert some data
 beforeAll( async () => {
-    const res = await request.post('/api/org/create').set(Auth).send(org[1]);
+    app.set('db', db);
+    const res = await supertest(app).post('/api/org/create').set(Auth).send(org[1]);
     project.orgId = res.body.org_id;
 });
 
@@ -27,18 +26,18 @@ afterEach(() => {
 
 describe('Projects', () => {
     it("Lists all Projects --> GET ", async () => {
-        const response = await request.get('/api/project').set(Auth);
+        const response = await supertest(app).get('/api/project').set(Auth);
         expect(response.statusCode).toBe(200)
     });
     it("Creates New Projects --> POST", async () => {
-        const response = await request.post('/api/project/create').set(Auth).send(project)
+        const response = await supertest(app).post('/api/project/create').set(Auth).send(project)
         expect(response.statusCode).toBe(201);
         expect(response.body).toHaveProperty("project_id");
     });
     it("Gets Project by ID", async () => {
-        const response = await request.post('/api/project/create').set(Auth).send(project);
+        const response = await supertest(app).post('/api/project/create').set(Auth).send(project);
         const project_id = response.body.project_id;
-        const getProjectById = await request.get(`/api/project/${project_id}`).set(Auth);
+        const getProjectById = await supertest(app).get(`/api/project/${project_id}`).set(Auth);
         expect(getProjectById.statusCode).toBe(200)
         expect(getProjectById.body).toHaveProperty("project_id");
         expect(getProjectById.body.project_id).toBe(project_id)
